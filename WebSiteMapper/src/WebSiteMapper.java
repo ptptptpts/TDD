431964a5fd7945765451359f1c1d7cc1435bc185
Taesoon Park <ptptptpts@gmail.com>
2021-05-13 09:07:26 +0900

    Implement retrieve, parse, getUrl, build test on WebSiteMapper
    Implement simple unit tests for below features and implement
    them on product code.
    
    - testRetrieveToWebsite: Retrieve website content to String
    - testParseHrefUrl: Find Href item with Url
    - testGetUrlFromHref: Get Url from Href item
    - testBuildWebSiteMap: Build web site tree from given url

--- WebSiteMapper/src/WebSiteMapper.java
+++ WebSiteMapper/src/WebSiteMapper.java
@@ -1,2 +1,81 @@
+import com.google.common.annotations.VisibleForTesting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WebSiteMapper {
+
+    public static class WebSiteNode {
+        String url;
+        ArrayList<WebSiteNode> childList;
+
+        public WebSiteNode(String url) {
+            this.url = url;
+        }
+    }
+
+    public WebSiteNode buildWebSiteMap(final String url) {
+        WebSiteNode root = new WebSiteNode(url);
+
+        String content = retrieveUrl(url);
+        HashSet<String> childUrls = fetchHrefLink(content);
+
+        root.childList = new ArrayList<>();
+        for (String childUrl : childUrls) {
+            root.childList.add(new WebSiteNode(childUrl));
+        }
+
+        return root;
+    }
+
+    public String retrieveUrl(final String link) {
+        StringBuilder content = new StringBuilder();
+
+        try (InputStream urlStream = new URL(link).openStream()) {
+            content = getContentFromUrl(urlStream);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return content.toString();
+    }
+
+    private StringBuilder getContentFromUrl(InputStream urlStream) throws IOException {
+        StringBuilder content = new StringBuilder();
+        BufferedReader urlReader = new BufferedReader(new InputStreamReader(urlStream));
+
+        String line;
+        while ((line = urlReader.readLine()) != null) {
+            content.append("\n");
+            content.append(line);
+        }
+
+        return content;
+    }
+
+    public HashSet<String> fetchHrefLink(final String content) {
+        HashSet<String> links = new HashSet<>();
+
+        Pattern pattern = Pattern.compile("href=([\"'])http(\\S|^\")*\"");
+        Matcher matcher = pattern.matcher(content);
+
+        while (matcher.find()) {
+            String hrefLink = matcher.group();
+            links.add(getUrlFromHref(hrefLink));
+        }
+
+        return links;
+    }
+
+    @VisibleForTesting
+    String getUrlFromHref(final String href) {
+        return href.split("=")[1].replace("\"", "");
+    }
 }
--- WebSiteMapper/test/WebSiteMapperTest.java
+++ WebSiteMapper/test/WebSiteMapperTest.java
@@ -1,5 +1,60 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebSiteMapperTest {
 
+    @Test
+    public void testRetrieveToWebsite() {
+        WebSiteMapper mapper = new WebSiteMapper();
+
+        String content = mapper.retrieveUrl("https://www.youtube.com");
+
+        assertNotNull(content);
+        assertNotEquals(0, content.length());
+    }
+
+    @Test
+    public void testParseHrefUrl() {
+        WebSiteMapper mapper = new WebSiteMapper();
+        String content = mapper.retrieveUrl("https://www.youtube.com");
+
+        HashSet<String> linkList = mapper.fetchHrefLink(content);
+
+        assertNotNull(linkList);
+        assertTrue(linkList.size() > 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "href=\"https://www.youtube.com/about/policies/\"",
+            "href=\"https://www.youtube.com/howyoutubeworks?utm_campaign=ytgen&amp;utm_source=ythp&amp;utm_medium=LeftNav&amp;utm_content=txt&amp;u=https%3A%2F%2Fwww.youtube.com%2Fhowyoutubeworks%3Futm_source%3Dythp%26utm_medium%3DLeftNav%26utm_campaign%3Dytgen\"",
+            "href=\"https://www.youtube.com/s/desktop/264d4061/cssbin/www-main-desktop-watch-page-skeleton.css\""
+    })
+    public void testGetUrlFromHref(String href) {
+        WebSiteMapper mapper = new WebSiteMapper();
+
+        String url = mapper.getUrlFromHref(href);
+
+        assertNotNull(url);
+        assertNotEquals(0, url.length());
+        assertTrue(url.startsWith("http"));
+        assertFalse(url.contains("\""));
+    }
+
+    @Test
+    public void testBuildWebSiteMap() {
+        WebSiteMapper mapper = new WebSiteMapper();
+        String testUrl = "https://www.youtube.com";
+
+        WebSiteMapper.WebSiteNode root = mapper.buildWebSiteMap(testUrl);
+
+        assertNotNull(root);
+        assertEquals(testUrl, root.url);
+        assertNotNull(root.childList);
+        assertTrue(root.childList.size() > 0);
+    }
 }
